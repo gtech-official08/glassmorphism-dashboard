e41c35b169a382fee911995b7271a0e170efc108
G-Tech Official <127587520+gtech-official08@users.noreply.github.com>
2024-09-10 20:18:00 +0545

    glassmorphism-design-starter-template

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { BrowserRouter as Router } from "react-router-dom"
+
+function App() {
+  return (
+    <>
+      <Router>
+        {/*  */}
+      </Router>
+    </>
+  )
+}
+
+export default App
